08c265f469a7446aad16a8356dfb86d0fae67f11
natnaelkora <natnaelkora@gmail.com>
2024-09-12 23:42:55 -0400

    home content

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import MyPicture from '../assets/picture.jpg';
 
 const Home = () => {
     return (
         <section style={styles.home}>
             <div style={styles.container}>
-                <h1 style={styles.heading}>Welcome to My Portfolio</h1>
-                <p style={styles.introduction}>
-                    I'm an IT Engineering graduate with experience in web development, Oracle Cloud administration,
-                    network design and implementation, and system design. Currently, I'm looking to further my education
-                    and specialize in Cyber Security and Cloud Administration.
-                </p>
+                <div style={styles.before_button}>
+                    <div style={styles.text_part}>
+                        <h1 style={styles.heading}>Hi!</h1>
+                        <h1 style={styles.heading}>I am Natnael (Nate)</h1>
+                        <p style={styles.introduction}>
+                            I'm an IT Engineering graduate with experience in web development, Oracle Cloud administration,
+                            network design and implementation, and system design. Currently, I'm looking to further my education
+                            and specialize in Cyber Security and Cloud Administration.
+                        </p>
+                    </div>
+                    <div style={styles.mypicture}>
+                        <img src={MyPicture} alt='Nate'></img>
+                    </div>
+
+                </div>
                 <a href="/portfolio" style={styles.ctaButton}>View My Work</a>
             </div>
-        </section>        
+        </section>
     );
 };
 
@@ -38,6 +48,20 @@ const styles = {
         fontSize: '20px',
         lineHeight: '1.6',
         marginBottom: '40px',
+    },
+    before_button: {
+        textAlign: 'left',
+        display: 'flex',
+    },
+    text_part: {
+        padding: 2,
+        flex: 1,
+    },
+    mypicture: {
+        fontSize: '20px',
+        lineHeight: '1.6',
+        marginBottom: '40px',
+        textAlign: 'left',
     },
     ctaButton: {
         padding: '10px 20px',
